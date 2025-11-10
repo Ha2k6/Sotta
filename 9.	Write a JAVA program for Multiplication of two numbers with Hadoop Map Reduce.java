d2374afef7e308e40874259bcbfdea6143430503
Mageswaran M <69040612+Ha2k6@users.noreply.github.com>
2025-11-10 11:18:13 +0530

    Implement Hadoop MapReduce for multiplication of two numbers

---
+++ 9.	Write a JAVA program for Multiplication of two numbers with Hadoop Map Reduce.java
@@ -0,0 +1,32 @@
+import org.apache.hadoop.io.*; 
+
+import org.apache.hadoop.mapreduce.*;
+
+
+
+public class Multiply {
+
+  public static class M extends Mapper<LongWritable,Text,Text,IntWritable>{
+
+    public void map(LongWritable k, Text v, Context c)throws java.io.IOException,InterruptedException{
+
+      String[] a=v.toString().split(" ");
+
+      c.write(new Text("Result"), new IntWritable(Integer.parseInt(a[0]) * Integer.parseInt(a[1])));
+
+    }
+
+  }
+
+  public static class R extends Reducer<Text,IntWritable,Text,IntWritable>{
+
+    public void reduce(Text k, Iterable<IntWritable> v, Context c)throws java.io.IOException,InterruptedException{
+
+      for(IntWritable i:v) c.write(k,i);
+
+    }
+
+  }
+
+}
+
